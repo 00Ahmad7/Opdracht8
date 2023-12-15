6d9f756e9871a2dbd41e55b9ca7686f7f16e80c5
FIRST_NAME LAST_NAME <EMAIL_ADDRESS>
2023-12-15 14:30:02 +0100

    Een score systeem gemaakt en de bug van de rare tekens opgelost

--- Opdracht 8/VragenPagina.xaml.cs
+++ Opdracht 8/VragenPagina.xaml.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Microsoft.Maui.Controls;
+using System.Net;
 
 namespace Opdracht_8
 {
     public partial class VragenPagina : ContentPage
     {
+        private double score = 0.0;
+        private int answeredQuestions = 0;
+        private int totalQuestions;
+        private int hardCorrectCount = 0;
+        private int mediumCorrectCount = 0;
+        private int easyCorrectCount = 0;
+
         public VragenPagina()
         {
             InitializeComponent();
@@ -21,30 +30,30 @@ namespace Opdracht_8
 
             try
             {
-
                 string json = await client.GetStringAsync(apiUrl);
                 TriviaData triviaData = JsonConvert.DeserializeObject<TriviaData>(json);
+                totalQuestions = triviaData.results.Count;
 
                 foreach (var question in triviaData.results)
                 {
-                    string decodedQuestion = question.question.Replace("&quot;", "\"");
+                    string decodedQuestion = WebUtility.HtmlDecode(question.question);
                     TriviaLayout.Children.Add(new Label { Text = decodedQuestion, FontSize = 18, Margin = new Thickness(20) });
-                    
+
                     foreach (var answer in question.RandomizedAnswers)
                     {
+                        var decodedAnswer = WebUtility.HtmlDecode(answer);
+
                         var button = new Button
                         {
-                            Text = answer,
+                            Text = decodedAnswer,
                             Margin = new Thickness(0, 0, 0, 10),
                             WidthRequest = 200,
                             BorderColor = Color.FromRgb(44, 196, 196),
                             TextColor = Color.FromRgb(0, 0, 0)
                         };
-                        button.Clicked += (s, e) => OnAnswerClicked(s, e, question, answer);
+                        button.Clicked += (s, e) => OnAnswerClicked(s, e, question, decodedAnswer);
                         TriviaLayout.Children.Add(button);
                     }
-
-
                 }
             }
             catch (Exception ex)
@@ -63,7 +72,24 @@ namespace Opdracht_8
             Button clickedButton = (Button)sender;
             if (selectedAnswer == question.correct_answer)
             {
-                // Als het antwoord correct is, zet de achtergrondkleur op groen
+                // Als het antwoord correct is, update de score op basis van de moeilijkheidsgraad
+                switch (question.difficulty.ToLower())
+                {
+                    case "hard":
+                        score += 1.0;
+                        hardCorrectCount++;
+                        break;
+                    case "medium":
+                        score += 0.5;
+                        mediumCorrectCount++;
+                        break;
+                    case "easy":
+                        score += 0.25;
+                        easyCorrectCount++;
+                        break;
+                }
+
+                // Zet de achtergrondkleur op groen
                 clickedButton.BackgroundColor = Color.FromRgb(0, 255, 0);
             }
             else
@@ -83,6 +109,20 @@ namespace Opdracht_8
             }
 
             question.HasAnswered = true;
+            answeredQuestions++;
+
+            // Controleer of alle vragen zijn beantwoord
+            if (answeredQuestions == totalQuestions)
+            {
+                ShowScorePopup();
+            }
+        }
+
+        private async void ShowScorePopup()
+        {
+            string explanation = $"Hard: {hardCorrectCount} correct, Medium: {mediumCorrectCount} correct, Easy: {easyCorrectCount} correct";
+            await DisplayAlert("Quiz Resultaat", $"Je score is: {score}\n\n{explanation}", "Ok");
+            await Navigation.PopAsync();
         }
 
     }
